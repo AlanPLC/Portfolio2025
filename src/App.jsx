f427632b9436e20977c9121cd22afd203dbb26fd
Simon Santarelli <simisantarelli@gmail.com>
2025-11-19 19:27:44 -0300

    App fondo - Presentation - About

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Presentation from './sections/Presentation/Presentation'
 import About from './sections/About/About'
+import Nav from "./components/Navbar/nav.jsx"
 
 function App() {
 
   return (
     <>
+      <Nav/>
       <Presentation/>
       <About/>
     </>
