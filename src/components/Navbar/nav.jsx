398b9d5ecd32341f27bd4f113dcc677298bdacc3
Alan Carrizo <alancarrizo1017@gmail.com>
2025-12-25 17:33:02 -0300

    Cambios provisorios al estilo de navbar, prueba

--- src/components/Navbar/nav.jsx
+++ src/components/Navbar/nav.jsx
@@ -102,64 +102,75 @@ function Nav() {
     }
   }, [active]);
 
-  const handleScroll = (id) => {
-    setIsOpen(false);
+    const handleScroll = (id) => {
     const section = sectionsRefs.current[id];
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
+    if (!section) return;
+
+    const closeNav = () => {
+      setVisible(true);
+      window.removeEventListener("scrollend", closeNav);
+    };
+
+    // Si el navegador soporta scrollend
+    window.addEventListener("scrollend", closeNav, { once: true });
+
+    section.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
     <>
       <nav className={`${visible ? "nav show" : "nav hide"} ${isOpen ? "menu-open" : ""}`}>
-        <div className="nav1" onClick={() => toogleLang()}>
-          <div className="icon-wrapper">
-            <img src="/project-icons/naver-dictionary-svgrepo-com.svg" className="icon icon-front" alt="lang" />
-            <img src="/project-icons/davx5-svgrepo-com.svg" className="icon icon-back" alt="lang" />
+        <div className="left-nav">
+          <div className="nav1" onClick={() => toogleLang()}>
+            <div className="icon-wrapper">
+              <img src="/project-icons/naver-dictionary-svgrepo-com.svg" className="icon icon-front" alt="lang" />
+              <img src="/project-icons/davx5-svgrepo-com.svg" className="icon icon-back" alt="lang" />
+            </div>
+            <p key={lang}>{lang === "es" ? "Español" : "English"}</p>
           </div>
-          <p key={lang}>{lang === "es" ? "Español" : "English"}</p>
-        </div>
-        <button className={`burger ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-
-        <div className={`nav2 ${isOpen ? "open" : ""}`}>
-          <ul>
-            {NAV_ITEMS.map((item, i) => (
-              <li
-                key={item.key}
-                className={active === item.section ? "active" : ""}
-                onClick={() => handleScroll(item.section)}
-              >
-                {translation.nav.navItems[i]}
-              </li>
-            ))}
-          </ul>
-          <span className="underline" style={underlineStyle}></span>
-        </div>
-
-        <div className="nav3">
-          <a href={link} target="_blank" rel="noopener noreferrer">
-            <img src="/project-icons/github-svgrepo-com.svg" alt="github" />
-          </a>
-          <button onClick={handleCopy}>
-            <img src="/project-icons/fairemail-svgrepo-com.svg" alt="email" />
+          <button className={`burger ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
           </button>
-          <a href={link} target="_blank" rel="noopener noreferrer">
-            <img src="/project-icons/linkedin-svgrepo-com.svg" alt="linkedin" />
-          </a>
-        </div>
 
-        {showModal && (
-          <div className={`copy-toast ${isExiting ? 'exit' : ''}`}>
-            <p>¡Correo copiado!</p>
-            <div className="progress-bar"></div>
+          <div className={`nav2 ${isOpen ? "open" : ""}`}>
+            <ul>
+              {NAV_ITEMS.map((item, i) => (
+                <li
+                  key={item.key}
+                  className={active === item.section ? "active" : ""}
+                  onClick={() => handleScroll(item.section)}
+                >
+                  {translation.nav.navItems[i]}
+                </li>
+              ))}
+            </ul>
+            <span className="underline" style={underlineStyle}></span>
           </div>
-        )}
+        </div>
+        <div className="right-nav">
+          <div className="nav3">
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              <img src="/project-icons/github-svgrepo-com.svg" alt="github" />
+            </a>
+            <button onClick={handleCopy}>
+              <img src="/project-icons/fairemail-svgrepo-com.svg" alt="email" />
+            </button>
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              <img src="/project-icons/linkedin-svgrepo-com.svg" alt="linkedin" />
+            </a>
+          </div>
+
+          {showModal && (
+            <div className={`copy-toast ${isExiting ? 'exit' : ''}`}>
+              <p>¡Correo copiado!</p>
+              <div className="progress-bar"></div>
+            </div>
+          )}
+        </div>
       </nav>
+
     </>
   );
 }
