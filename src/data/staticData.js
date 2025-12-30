066125b0e23b26718af1bfc3c28463862bb31336
Alan Carrizo <alancarrizo1017@gmail.com>
2025-12-30 16:48:29 -0300

    Modificación rastreo personal data

---
+++ src/data/staticData.js
@@ -0,0 +1,82 @@
+export const profile = {
+  name: "Alan",
+  avatar: "/projects-imgs/avatar.jpg",
+  links: {
+    cv: "/assets/CV-Simon.pdf",
+    contact: "alancarrizo1017@gmail.com",
+    github: "https://github.com/AlanPLC",
+    linkedin: "https://www.linkedin.com/in/alan-pablo-lautaro-carrizo-795968246/",
+  },
+  stats: {
+    experience: "2+",
+    skills: "7+",
+    projects: "3+"
+  }
+};
+
+export const experienceData = {
+  experience1:{
+    img: "./projects-imgs/vector.png",
+    tech: ["TypeScript", "Node.js", "Make ", "Webhooks", "WhatsApp Business API", "Docker"],
+  },
+};
+
+export const projectsData = {
+  project1: {
+    img: "./projects-imgs/minimarket.png",
+    tech: ["React", "Node.js", "Vite", "MySQL", "Express.js"],
+    // liveUrl:"lol",
+    codeUrl:"https://github.com/AlanPLC/TP-Final-Lab.-IV",
+  },
+  project2: {
+    img: "./projects-imgs/poke.png",
+    tech: ["ReactNative", "Expo"],
+    codeUrl:"https://github.com/AlanPLC/Pokedex",
+  },
+};
+
+// "React", "Node.js", "Express", "Mysql", "ReactNative"
+export const toolsData = {
+  react: {
+    name: "React",
+    complement: "Vite",
+    techs: ["React", "Vite"],
+    percentage: 90,
+  },
+  reactNative: {
+    name: "React Native",
+    complement: "Expo",
+    techs: ["R Native", "Expo"],
+    percentage: 60,
+  },
+  javascript: {
+    name: "Javascript",
+    complement: "",
+    techs: ["Javascript"],
+    percentage: 80,
+  },
+  mysql: {
+    name: "MySQL",
+    complement: "",
+    techs: ["MySQL"],
+    percentage: 60,
+  },
+  nodejs: {
+    name: "NodeJS",
+    complement: "Express",
+    techs: ["NodeJS", "Express"],
+    percentage: 65,
+  },
+  git: {
+    name: "GIT",
+    complement: "Github",
+    techs: ["GIT", "Github"],
+    percentage: 100,
+  },
+  python: {
+    name: "Python",
+    complement: "PyQT",
+    techs: ["Python", "PyQT"],
+    percentage: 40,
+  }
+};
