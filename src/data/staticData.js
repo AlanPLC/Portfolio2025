644ea8f894295e0f2e2b3560d14af690e8187007
Simon Santarelli <simisantarelli@gmail.com>
2026-03-01 16:25:24 -0300

    feat: add LinkedIn, GitHub repos, and Redsync experience

---
+++ src/data/staticData.js
@@ -0,0 +1,146 @@
+export const profile = {
+  name: "Simón",
+  avatar: "/avatar.jpg",
+  links: {
+    cv: "/assets/CV-Simon.pdf",
+    contact: "simisantarelli@gmail.com",
+    github: "https://github.com/saiisanta",
+    linkedin: "https://www.linkedin.com/in/simon-santarelli-6a5994399/",
+  },
+  stats: {
+    experience: "2+",
+    skills: "7+",
+    projects: "4+"
+  }
+};
+
+export const experienceData = {
+  experience1: {
+    tech: ["React", ".NET", "ASP.NET", "SQL Server"],
+    impactLevel: 5,
+  },
+  experience2:{
+    tech: ["JS","React", "Express.js", "MySQL", "SCSS" ],
+    impactLevel: 4,
+  },
+  experience3:{
+    tech: ["JS","React","Electron","Express.js", "PostgreSQL", "SCSS"],
+    impactLevel: 4,
+  },
+};
+
+export const projectsData = {
+  project1: {
+    img: "./projects-imgs/highfit.svg",
+    tech: ["React", ".NET", "ASP.NET", "SQL Server"],
+    impactLevel: 5,
+    "codeUrl": {
+      "front": "https://github.com/saiisanta/gym-management-front",
+      "back": "https://github.com/saiisanta/gym-management-backend"
+    },  
+  },
+  project2: {
+    img: "./projects-imgs/rifli.svg",
+    tech: ["JS","React", "Express.js", "MySQL", "SCSS" ],
+    impactLevel: 4,
+    "codeUrl": {
+      "front": "https://github.com/saiisanta/RIFLI-FRONT",
+      "back": "https://github.com/saiisanta/RIFLI-BACK"
+    },  
+  },
+  project3:{
+    img: "./projects-imgs/redsync.svg",
+    tech: ["JS","React","Electron","Express.js", "PostgreSQL", "SCSS"],
+    impactLevel: 4,
+    "codeUrl": {
+      "front": "https://github.com/saiisanta/redsync",
+      "back": "https://github.com/saiisanta/redsync"
+    },  
+  }
+};
+export const toolsData = {
+  javascript: {
+    name: "JavaScript",
+    complement: "ES6+",
+    techs: ["JavaScript"],
+    percentage: 80,
+  },
+  react: {
+    name: "React",
+    complement: "Frontend",
+    techs: ["React", "Vite"],
+    percentage: 90,
+  },
+  csharp: {
+    name: "C#",
+    complement: "Backend",
+    techs: ["C#", ".NET Core"],
+    percentage: 75,
+  },
+  sql: {
+    name: "Bases de Datos",
+    complement: "Relacional",
+    techs: ["SQL", "MySQL"],
+    percentage: 80,
+  },
+  nodejs: {
+    name: "Node.js",
+    complement: "Runtime",
+    techs: ["Node.js", "Express"],
+    percentage: 70,
+  },
+  mongoDB: {
+    name: "MongoDB",
+    complement: "NoSQL",
+    techs: ["MongoDB", "Mongoose"],
+    percentage: 60,
+  },
+  postgresql: {
+    name: "PostgreSQL",
+    complement: "Relacional",
+    techs: ["Postgre", "DBeaver"],
+    percentage: 55,
+  },
+  git: {
+    name: "Version Control",
+    complement: "DevOps",
+    techs: ["Git", "GitHub"],
+    percentage: 100,
+  },
+  c: {
+    name: "C",
+    complement: "Low Level",
+    techs: ["C"],
+    percentage: 55,
+  },
+  cpp: {
+    name: "C++",
+    complement: "Low Level",
+    techs: ["C++"],
+    percentage: 75,
+  },
+  py: {
+    name: "Py",
+    complement: "Low Level",
+    techs: ["Python"],
+    percentage: 55,
+  },
+  python: {
+    name: "Python",
+    complement: "Data Science",
+    techs: ["Python", "Pandas"],
+    percentage: 65,
+  },
+  styling: {
+    name: "Maquetación",
+    complement: "Diseño",
+    techs: ["Sass", "Flex"],
+    percentage: 100,
+  },
+  logic: {
+    name: "Arquitectura",
+    complement: "Análisis",
+    techs: ["UML", "Doc"],
+    percentage: 80,
+  }
+};
